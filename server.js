38599bda262c14b1d44341ca63be832e9d53fc75
Àlex Serra <bounteous17@protonmail.com>
2022-06-24 11:25:04 +0200

    simple socket-io server for movement

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+
+const ioServer = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+});
+
+// Socket app msgs
+ioServer.on('connection', (client) => {
+  console.log(
+    `User ${client.id} connected, there are currently ${ioServer.engine.clientsCount} users connected`
+  )
+
+  //Add a new client indexed by his id
+  clients[client.id] = {
+    position: [0, 0, 0],
+    rotation: [0, 0, 0],
+  }
+
+  ioServer.sockets.emit('move', clients)
+
+  client.on('move', ({ id, rotation, position }) => {
+    clients[id].position = position
+    clients[id].rotation = rotation
+
+    ioServer.sockets.emit('move', clients)
+  })
+
+  client.on('disconnect', () => {
+    console.log(
+      `User ${client.id} disconnected, there are currently ${ioServer.engine.clientsCount} users connected`
+    )
+
+    //Delete this client from the object
+    delete clients[client.id]
+
+    ioServer.sockets.emit('move', clients)
+  })
+});
